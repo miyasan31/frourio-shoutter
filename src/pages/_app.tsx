534e920bb62936f4a34745a3d1885431bdb69532
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-14 00:05:52 +0900

    ✨ ChakraUI追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,16 @@ import '~/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
 import { memo } from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '~/utils/theme'
 
 const MyApp = (props: AppProps) => {
   return (
-    <RecoilRoot>
-      <props.Component {...props.pageProps} />
-    </RecoilRoot>
+    <ChakraProvider theme={theme}>
+      <RecoilRoot>
+        <props.Component {...props.pageProps} />
+      </RecoilRoot>
+    </ChakraProvider>
   )
 }
 
