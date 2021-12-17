c8991390586706e03f580d5d3d4da2405b79a962
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-17 10:31:04 +0900

    ✨ /api/like/:id

---
+++ server/api/like/_likeId@number/controller.ts
@@ -0,0 +1,14 @@
+import { defineController } from './$relay'
+import { deleteLike } from '$/service/like'
+
+export default defineController(() => ({
+  delete: async ({ params }) => {
+    const like = await deleteLike(params.likeId)
+
+    if (!like) {
+      return { status: 400 }
+    }
+
+    return { status: 204 }
+  }
+}))
