b5854d440ddd77d48a73a1f35beeff719ff7bff3
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-17 12:38:19 +0900

    ✨ di/user/tweet

--- server/api/user/_userId@string/tweet/controller.ts
+++ server/api/user/_userId@string/tweet/controller.ts
@@ -1,14 +1,17 @@
 import { getUserTweetList } from '$/service/user'
 import { defineController } from './$relay'
 
-export default defineController(() => ({
-  get: async ({ params }) => {
-    const tweetlist = await getUserTweetList(params.userId)
-
-    if (tweetlist.length === 0) {
-      return { status: 404 }
+export default defineController(
+  {
+    getUserTweetList
+  },
+  ({ getUserTweetList }) => ({
+    get: async ({ params }) => {
+      const tweetlist = await getUserTweetList(params.userId)
+      if (tweetlist.length === 0) {
+        return { status: 404 }
+      }
+      return { status: 200, body: tweetlist }
     }
-
-    return { status: 200, body: tweetlist }
-  }
-}))
+  })
+)
