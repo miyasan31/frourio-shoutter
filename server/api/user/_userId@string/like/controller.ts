5f33fa99c1f064dc4b056ca223ba7ff021234fb0
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-17 12:37:53 +0900

    ✨ di/user/like

--- server/api/user/_userId@string/like/controller.ts
+++ server/api/user/_userId@string/like/controller.ts
@@ -1,14 +1,17 @@
 import { getUserLikeList } from '$/service/user'
 import { defineController } from './$relay'
 
-export default defineController(() => ({
-  get: async ({ params }) => {
-    const likelist = await getUserLikeList(params.userId)
-
-    if (likelist.length === 0) {
-      return { status: 404 }
+export default defineController(
+  {
+    getUserLikeList
+  },
+  ({ getUserLikeList }) => ({
+    get: async ({ params }) => {
+      const likelist = await getUserLikeList(params.userId)
+      if (likelist.length === 0) {
+        return { status: 404 }
+      }
+      return { status: 200, body: likelist }
     }
-
-    return { status: 200, body: likelist }
-  }
-}))
+  })
+)
