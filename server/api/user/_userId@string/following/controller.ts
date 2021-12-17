b77b7fe1e64f7704537ede4c4bf68349b3f9fed4
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-17 10:33:10 +0900

    ✨ /api/user/:id/following

---
+++ server/api/user/_userId@string/following/controller.ts
@@ -0,0 +1,14 @@
+import { defineController } from './$relay'
+import { getUserFollowingList } from '$/service/user'
+
+export default defineController(() => ({
+  get: async ({ params }) => {
+    const followinglist = await getUserFollowingList(params.userId)
+
+    if (followinglist.length === 0) {
+      return { status: 404 }
+    }
+
+    return { status: 200, body: followinglist }
+  }
+}))
---
+++ server/api/user/_userId@string/following/index.ts
@@ -0,0 +1,11 @@
+import type { Follow, User } from '$prisma/client'
+
+export type Methods = {
+  get: {
+    resBody: (User & {
+      followings: (Follow & {
+        follower: User
+      })[]
+    })[]
+  }
+}
