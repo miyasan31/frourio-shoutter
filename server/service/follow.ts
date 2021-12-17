fded5f48386f76cebcaa1557d1bda5044ecdc0da
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-17 12:26:27 +0900

    ✨ di/follow

--- server/service/follow.ts
+++ server/service/follow.ts
@@ -1,16 +1,39 @@
+import { depend } from 'velona'
 import { PrismaClient } from '@prisma/client'
 import type { Follow, Prisma } from '$prisma/client'
 
 const prisma = new PrismaClient()
 
-export const createFollow = async (createFollow: Prisma.FollowCreateInput) => {
-  const result = await prisma.follow.create({ data: createFollow })
+export const createFollow = depend(
+  {
+    prisma: prisma as unknown as {
+      follow: {
+        create(query: Prisma.FollowCreateArgs): Promise<Follow>
+      }
+    }
+  },
+  async ({ prisma }, createFollow: Prisma.FollowCreateInput) => {
+    const result = await prisma.follow.create({
+      data: createFollow
+    })
+    return result
+  }
+)
 
-  return result
-}
-
-export const deleteFollow = async (id: Follow['id']) => {
-  const result = await prisma.follow.delete({ where: { id } })
-
-  return result
-}
+export const deleteFollow = depend(
+  {
+    prisma: prisma as unknown as {
+      follow: {
+        delete(query: Prisma.FollowDeleteArgs): Promise<Follow>
+      }
+    }
+  },
+  async ({ prisma }, id: Follow['id']) => {
+    const result = await prisma.follow.delete({
+      where: {
+        id: id
+      }
+    })
+    return result
+  }
+)
