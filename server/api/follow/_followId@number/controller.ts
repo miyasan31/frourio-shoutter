0fb3ba4c8e780710debb858dc4662e576f948c3e
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-17 10:30:07 +0900

    ✨ /api/follow/:id

---
+++ server/api/follow/_followId@number/controller.ts
@@ -0,0 +1,14 @@
+import { defineController } from './$relay'
+import { deleteFollow } from '$/service/follow'
+
+export default defineController(() => ({
+  delete: async ({ params }) => {
+    const follow = await deleteFollow(params.followId)
+
+    if (!follow) {
+      return { status: 400 }
+    }
+
+    return { status: 204 }
+  }
+}))
