f7ee2bad67891a0627c08f1e5ae745e8a60eadd7
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-17 12:39:06 +0900

    ✨ di/user/:id

--- server/api/user/_userId@string/controller.ts
+++ server/api/user/_userId@string/controller.ts
@@ -1,14 +1,17 @@
 import { defineController } from './$relay'
 import { getUser } from '$/service/user'
 
-export default defineController(() => ({
-  get: async ({ params }) => {
-    const user = await getUser(params.userId)
-
-    if (!user) {
-      return { status: 404 }
+export default defineController(
+  {
+    getUser
+  },
+  ({ getUser }) => ({
+    get: async ({ params }) => {
+      const user = await getUser(params.userId)
+      if (!user) {
+        return { status: 404 }
+      }
+      return { status: 200, body: await user }
     }
-
-    return { status: 200, body: await user }
-  }
-}))
+  })
+)
