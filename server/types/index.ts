7b3eb7525a4445f4564a4ae1b42e7629c4278983
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-17 10:35:25 +0900

    🏷️

--- server/types/index.ts
+++ server/types/index.ts
@@ -7,3 +7,64 @@ export type UserInfo = {
 export type AuthHeader = {
   authorization: string
 }
+
+export type User = {
+  id: string
+  name: string
+  profile: string
+  createdAt: string
+  updatedAt: string
+  tweet?: Tweet[]
+  like?: Like[]
+  reply?: Reply[]
+  retweet?: Retweet[]
+  follower?: Follow[]
+  following?: Follow[]
+}
+
+export type Tweet = {
+  id: number
+  userId: string
+  tweet: string
+  createdAt: string
+  user?: User
+  like?: Like[]
+  reply?: Reply[]
+  retweet?: Retweet[]
+}
+
+export type Reply = {
+  id: number
+  userId: string
+  tweetId: number
+  reply: string
+  createdAt: string
+  user?: User
+}
+
+export type Like = {
+  id: number
+  userId: string
+  tweetId: number
+  createdAt: string
+  user?: User
+  tweet?: Tweet
+}
+
+export type Retweet = {
+  id: number
+  userId: string
+  tweetId: number
+  createdAt: string
+  user?: User
+  tweet?: Tweet
+}
+
+export type Follow = {
+  id: number
+  userId: string
+  followId: string
+  createdAt: string
+  follower?: User
+  following?: User
+}
