ae4a300f625a646a89dc22b22c2603393de9e251
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-17 12:04:12 +0900

    ✨ di/tweet

--- server/api/tweet/controller.ts
+++ server/api/tweet/controller.ts
@@ -2,21 +2,27 @@ import { defineController } from './$relay'
 import { getTweetList, createTweet } from '$/service/tweet'
 
 // @/api/tweet
-export default defineController(() => ({
-  // not used
-  get: async () => {
-    const tweetlist = await getTweetList()
-    if (tweetlist.length === 0) {
-      return { status: 404 }
-    }
-    return { status: 200, body: tweetlist }
+export default defineController(
+  {
+    getTweetList,
+    createTweet
   },
+  ({ getTweetList, createTweet }) => ({
+    // not used
+    get: async () => {
+      const tweetlist = await getTweetList()
+      if (tweetlist.length === 0) {
+        return { status: 404 }
+      }
+      return { status: 200, body: tweetlist }
+    },
 
-  post: async ({ body }) => {
-    const tweet = await createTweet(body)
-    if (!tweet) {
-      return { status: 400 }
+    post: async ({ body }) => {
+      const tweet = await createTweet(body)
+      if (!tweet) {
+        return { status: 400 }
+      }
+      return { status: 201, body: tweet }
     }
-    return { status: 201, body: tweet }
-  }
-}))
+  })
+)
