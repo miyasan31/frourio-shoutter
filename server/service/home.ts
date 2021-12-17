67376a8ba2d699b3d6257ae0f334c29cf550ae36
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-17 13:06:10 +0900

    ✨ di/home

--- server/service/home.ts
+++ server/service/home.ts
@@ -1,43 +1,54 @@
+import { depend } from 'velona'
 import { PrismaClient } from '@prisma/client'
-import type { Tweet } from '$prisma/client'
+import type { Tweet, Prisma } from '$prisma/client'
+import { GetHome } from '$/types/home'
 
 const prisma = new PrismaClient()
 
-export const getFollowingUserTweetList = async (userId: Tweet['userId']) => {
-  const result = await prisma.follow.findMany({
-    where: {
-      // is following user
-      userId: userId
-    },
-    include: {
-      // following -> user
-      following: {
-        include: {
-          // user -> tweets
-          tweets: {
-            include: {
-              _count: {
-                select: {
-                  replies: true,
-                  retweets: true,
-                  likes: true
+export const getFollowingUserTweetList = depend(
+  {
+    prisma: prisma as unknown as {
+      follow: {
+        findMany(query: Prisma.FollowFindManyArgs): Promise<GetHome>
+      }
+    }
+  },
+  async ({ prisma }, userId: Tweet['userId']) => {
+    const result = await prisma.follow.findMany({
+      where: {
+        // is following user
+        userId: userId
+      },
+      include: {
+        // following -> user
+        following: {
+          include: {
+            // user -> tweets
+            tweets: {
+              include: {
+                _count: {
+                  select: {
+                    replies: true,
+                    retweets: true,
+                    likes: true
+                  }
                 }
               }
-            }
-          },
-          // user -> replies
-          replies: true,
-          // user -> retweets
-          retweets: {
-            include: {
-              // retweets -> tweet
-              tweet: {
-                include: {
-                  _count: {
-                    select: {
-                      replies: true,
-                      retweets: true,
-                      likes: true
+            },
+            // user -> replies
+            replies: true,
+            // user -> retweets
+            retweets: {
+              include: {
+                // retweets -> tweet
+                tweet: {
+                  include: {
+                    _count: {
+                      select: {
+                        replies: true,
+                        retweets: true,
+                        likes: true
+                      }
                     }
                   }
                 }
@@ -46,8 +57,7 @@ export const getFollowingUserTweetList = async (userId: Tweet['userId']) => {
           }
         }
       }
-    }
-  })
-
-  return result
-}
+    })
+    return result
+  }
+)
