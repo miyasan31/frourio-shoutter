93fd4e97f8255931b2aad27b01a9e6bcdfdcc340
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-19 14:34:53 +0900

    🚧

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,39 +1,38 @@
 import React, { FC, useEffect } from 'react'
-import useAspidaSWR from '@aspida/swr'
-import { apiClient } from '~/utils/apiClient'
-import { useRecoilState } from 'recoil'
-import { user } from '~/atoms'
+// import useAspidaSWR from '@aspida/swr'
+// import { apiClient } from '~/utils/apiClient'
+// import { useRecoilState } from 'recoil'
+// import { user } from '~/atoms'
+// import { useGetAccessToken } from '~/hooks/useGetAccessToken'
+// import { auth0 } from '~/constants/auth0'
 
 type Props = {
   children: React.ReactNode
 }
 
 export const AuthProvider: FC<Props> = (props) => {
-  const { data: authUser } = useAspidaSWR(
-    apiClient.user._userId('miyasan_0301')
-  )
+  // const [userInfo, setUserInfo] = useRecoilState(user)
+  // const { token } = useGetAccessToken()
 
-  const [userInfo, setUserInfo] = useRecoilState(user)
+  // const { data: authUser } = useAspidaSWR(
+  //   apiClient.user._userId('miyasan_0301'),
+  //   {
+  //     headers: { authorization: `Bearer ${token}` },
+  //     enabled: !!token
+  //   }
+  // )
 
-  useEffect(() => {
-    if (authUser) {
-      setUserInfo({
-        id: authUser.id,
-        name: authUser.name,
-        profile: authUser.profile,
-        createdAt: authUser.createdAt,
-        isSignin: true
-      })
-    }
-  }, [authUser])
+  // useEffect(() => {
+  //   if (authUser) {
+  //     setUserInfo({
+  //       id: authUser.id,
+  //       name: authUser.name,
+  //       profile: authUser.profile,
+  //       createdAt: authUser.createdAt,
+  //       isSignin: true
+  //     })
+  //   }
+  // }, [authUser])
 
-  return (
-    <>
-      {userInfo.isSignin ? (
-        <div>{props.children}</div>
-      ) : (
-        <div>ログインしてください</div>
-      )}
-    </>
-  )
+  return <>{props.children}</>
 }
