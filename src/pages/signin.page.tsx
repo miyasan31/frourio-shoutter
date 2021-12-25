e47cadd0f7f515b200659296776e082d554cefbb
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-26 03:36:40 +0900

    🚧

--- src/pages/signin.page.tsx
+++ src/pages/signin.page.tsx
@@ -1,12 +1,13 @@
 import { Box, Button } from '@chakra-ui/react';
 import { NextPage } from 'next';
 
-import { handleSignin } from '~/functions';
+import { handleSignin, handleSignout } from '~/functions';
 
 const SigninPage: NextPage = () => {
   return (
     <Box w="100%" py="1rem">
       <Button onClick={handleSignin}>サインインする</Button>
+      <Button onClick={handleSignout}>サインアウトする</Button>
     </Box>
   );
 };
