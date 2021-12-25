b1d6d9a9e9d6229481890a9541ad7be38bdf32c2
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-26 03:40:09 +0900

    ✨ revalidate追加

--- src/components/form/TweetForm.tsx
+++ src/components/form/TweetForm.tsx
@@ -1,18 +1,26 @@
+import useAspidaSWR from '@aspida/swr';
 import { FC, useCallback } from 'react';
 import { useRecoilValue } from 'recoil';
 
 import { user } from '~/atoms';
 import { getToken } from '~/functions';
+import { useGetAccessToken } from '~/hooks';
 import { apiClient } from '~/utils';
 
 import { Form } from './Form';
 
-type Props = {
-  revalidate?: () => Promise<boolean>;
-};
-
-export const TweetForm: FC<Props> = (props) => {
+export const TweetForm: FC = () => {
   const userInfo = useRecoilValue(user);
+  const { token } = useGetAccessToken();
+
+  const { revalidate } = useAspidaSWR(
+    apiClient.home._requestUserId(userInfo.id),
+    {
+      headers: { authorization: `Bearer ${token}` },
+      enabled: !!token && !!userInfo.id
+      // refreshInterval: 1000
+    }
+  );
 
   const handlePostTweet = useCallback(
     async (data) => {
@@ -24,9 +32,9 @@ export const TweetForm: FC<Props> = (props) => {
           user: { connect: { id: userInfo.id } }
         }
       });
-      props.revalidate ? props.revalidate() : null;
+      revalidate();
     },
-    [userInfo]
+    [userInfo, revalidate]
   );
 
   return <Form type="tweet" onPost={handlePostTweet} />;
