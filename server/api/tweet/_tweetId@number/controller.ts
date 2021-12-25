f227c63d9dda904a5b5798e2add15718f5c503b7
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-25 23:49:07 +0900

    ♻️ controllerレベルのhooksに変更

--- server/api/tweet/_tweetId@number/controller.ts
+++ server/api/tweet/_tweetId@number/controller.ts
@@ -1,6 +1,27 @@
 import { deleteTweet, getTweet, updateTweet } from '$/service/tweet';
+import { getSignupUserCheck, getUserInfo } from '$/service/user';
+import type { RequestUserInfo } from '$/types';
+import type { User } from '$prisma/client';
 
-import { defineController } from './$relay';
+import { defineController, defineHooks } from './$relay';
+
+export type AdditionalRequest = {
+  userInfo: User;
+};
+
+// tokenからuserを取得する
+export const hooks = defineHooks(() => ({
+  onRequest: async (request, _reply, done) => {
+    // GET以外は何もしない
+    if (request.method !== 'GET') return;
+
+    const token = request?.headers.authorization;
+    const user = await getUserInfo<RequestUserInfo>(token).then((res) => res);
+    const userData = await getSignupUserCheck(user.email);
+    request.userInfo = userData;
+    done();
+  }
+}));
 
 // @/api/tweet/:id
 export default defineController(
